f4dba7c786b9e05ef3028874f28898c2ccdc24e9
Mr. Coxall <patrick.coxall@ocsb.ca>
2021-02-17 16:50:49 +0000

    linting fixes

--- js/menuScene.js
+++ js/menuScene.js
@@ -6,23 +6,27 @@ class MenuScene extends Phaser.Scene {
   }
 
   init () {
-
   }
 
   preload () {
-
+    this.load.image('start', 'assets/start.png')
   }
 
   create () {
     this.scene.remove('titleScene')
+
+    const startButton = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'start').setInteractive()
+    startButton.on('pointerdown', () => this.clickButton())
   }
 
   update () {
-    this.scene.switch('gameScene')
   }
 
   end () {
+  }
 
+  clickButton () {
+    this.scene.switch('gameScene')
   }
 }
 
--- js/titleScene.js
+++ js/titleScene.js
@@ -6,12 +6,10 @@ class TitleScene extends Phaser.Scene {
   }
 
   init (data) {
-
   }
 
   preload () {
     this.load.image('background', 'assets/splashSceneImage.png')
-    this.load.image('start', 'assets/start.png')
   }
 
   create (data) {
@@ -20,20 +18,15 @@ class TitleScene extends Phaser.Scene {
     }
     const backgroundImage = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'background')
     backgroundImage.depth = 0
-    const startButton = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'start').setInteractive()
-    startButton.on('pointerdown', () => this.clickButton())
-    startButton.depth = 1
   }
 
   update (time, delta) {
     if (time > 4000) {
-      this.clickButton()
+      this.scene.switch('menuScene')
     }
   }
 
-  clickButton () {
-    console.log('Clicked')
-    this.scene.switch('menuScene')
+  end () {
   }
 }
 
