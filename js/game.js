24d8aead225d42703dbcf7ff4141c3112d60500c
Mr. Coxall <patrick.coxall@ocsb.ca>
2021-02-17 02:49:25 +0000

    added first scene

--- js/game.js
+++ js/game.js
@@ -1,9 +1,15 @@
 /* global Phaser */
+import TitleScene from './titleScene.js'
+
+// Our game scenes
+//var gameScene = new GameScene();
+var titleScene = new TitleScene();
 
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  backgroundColor: 0xffffff,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
@@ -11,5 +17,9 @@ const config = {
 }
 const game = new Phaser.Game(config)
 
+// load scenes
+game.scene.add('titleScene', titleScene)
+//game.scene.add("game", gameScene)
+
 // start title
-game.scene.start()
+game.scene.start('titleScene')
---
+++ js/titleScene.js
@@ -0,0 +1,37 @@
+class TitleScene extends Phaser.Scene {
+
+	constructor() {
+		super({key:'titleScene'})
+	}
+
+  init(data) {
+
+  }
+
+	preload() {
+		this.load.image('background', 'assets/splashSceneImage.png')
+    this.load.image('start', 'assets/start.png')
+	}
+
+	create(data) {
+    if (this.sound.context.state === 'suspended') {
+      this.sound.context.resume()
+    }
+    var backgroundImage = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY,'background')
+
+    var startButton = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY,'start').setInteractive()
+    startButton.on('pointerdown', () => this.clickButton())
+	}
+
+  update(time, delta) {
+
+	}
+
+  clickButton() {
+    console.log("Clicked")
+    this.scene.switch('gameScene')
+  }
+
+}
+
+export default TitleScene
